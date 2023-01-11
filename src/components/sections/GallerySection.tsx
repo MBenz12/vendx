68e309b8a33e7180e260711fccf305638a710198
mbenz12 <mbenz10120112@gmail.com>
2023-01-11 00:47:20 -0500

    gallery section animation added

--- src/components/sections/GallerySection.tsx
+++ src/components/sections/GallerySection.tsx
@@ -1,5 +1,7 @@
+import RevealText from "components/RevealText";
 import { CircleLeftIcon, CircleRightIcon, PolygonIcon, TriangleIcon } from "components/svgs";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useInViewport } from "react-in-viewport";
 
 const ArtCard = ({ image, style }: { image: string, style: any }) => {
     return (
@@ -22,6 +24,11 @@ const GallerySection = () => {
         '/art1.png',
     ]);
     const [active, setActive] = useState(2);
+    const textRef = useRef<any>();
+    const { inViewport } = useInViewport(textRef);
+    useEffect(() => {
+        RevealText();
+    }, [inViewport])
     return (
         <section className="section !bg-primary items-center container" id='gallery'>
             <div className="absolute left-[98px] top-0 translate-y-[-50%]">
@@ -52,14 +59,18 @@ const GallerySection = () => {
             </div>
             <div className='flex max-w-[1400px] w-full justify-between items-center md:flex-col md:gap-[100px] my-3'>
                 <div>
-                    <p className="max-w-[469px] w-full text-[60px] leading-[68px] font-title font-extrabold lg:text-4xl">{"// ARTWORK"}</p>
+                    <div className={"relative" + (inViewport ? " fo-mask" : "")}>
+                        <span className="black">
+                            <p className="max-w-[469px] w-full text-[60px] leading-[68px] font-title font-extrabold lg:text-4xl">{"// ARTWORK"}</p>
+                        </span>
+                    </div>
                     <div className="mt-[10px]">
                         <svg width="154" height="9" viewBox="0 0 154 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M61.8425 0.389893L49.5102 8.61033H0.402771V0.389893H61.8425H154H61.8425Z" fill="black" />
                             <path d="M61.8425 0.389893L49.5102 8.61033H0.402771V0.389893H61.8425ZM61.8425 0.389893H154" stroke="black" strokeWidth="2" strokeMiterlimit="10" />
                         </svg>
                     </div>
-                    <p className="mt-[35px] max-w-[374px] w-full font-primary text-secondary text-[36px] leading-[42px] lg:text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
+                    <p ref={textRef} className={"mt-[35px] max-w-[374px] w-full font-primary text-secondary text-[36px] leading-[42px] lg:text-2xl" + (inViewport ? " reveal-text" : "")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, </p>
                 </div>
                 <div className="carousel mx-[180px] flex-shrink-0 sm:mx-0">
                     {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><CircleLeftIcon /></button>}
