68710d78c512a93adbfb273f8b3b62ad6bb66ef6
mbenz12 <mbenz10120112@gmail.com>
2023-01-10 23:13:57 -0500

    magrin added to roadmap section

--- src/components/sections/RoadmapSection.tsx
+++ src/components/sections/RoadmapSection.tsx
@@ -2,8 +2,8 @@ const RodmapBox = () => {
     return (
         <div className="rounded-[15px] hover:p-[5px] max-w-[512px] mx-auto w-full bg-gradient1-hover hover:border-0 border-2 border-[#1A1A1A]" style={{ aspectRatio: 1.25 }}>
             <div className="w-full h-full bg-gradient2 bg-black rounded-[15px] flex flex-col justify-center items-center">
-                <img src="/vending_machine.png" alt="" className="w-[188px] sm:w-40" />
-                <p className="font-secondary text-secondary text-[30px] leading-[38px]">Lorem Ipsum dolor</p>
+                <img src="/vending_machine.png" alt="" className="w-[188px] sm:w-[120px]" />
+                <p className="font-secondary text-secondary text-[30px] sm:text-[24px] leading-[38px] sm:leading-[28px]">Lorem Ipsum dolor</p>
             </div>
         </div>
     )
@@ -13,7 +13,7 @@ const RoadmapSection = () => {
     return (
         <section className="section flex-col gap-[90px]" id='roadmap'>
             <div className='container my-10'>
-                <div className="ml-[5vw]">
+                <div className="ml-10">
                     <div className="w-fit font-extrabold font-title text-[80px] md:text-[60px] sm:text-[40px] xs:text-3xl leading-[115px] md:leading-[75px] sm:leading-[45px] text-white">
                         <div className="fo-mask relative">
                             <span className="white">
@@ -25,7 +25,7 @@ const RoadmapSection = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                     </p>
                 </div>
-                <div className="w-full grid grid-cols-3 items-center justify-center gap-[25px] my-10 lg:grid-cols-2 sm:grid-cols-1">
+                <div className="w-full sm:px-5 px-10 grid grid-cols-3 items-center justify-center gap-[25px] my-10 lg:grid-cols-2 sm:grid-cols-1">
                     <RodmapBox />
                     <RodmapBox />
                     <RodmapBox />
